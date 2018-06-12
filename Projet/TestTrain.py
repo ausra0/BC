46d6fd354c3105ef5f951b9550d86938fab424d3
ausra0 <aushra.pogozelskyte@gmail.com>
2018-06-12 11:06:38 +0200

    update TestTrain with various parameter options

--- Projet/TestTrain.py
+++ Projet/TestTrain.py
@@ -11,10 +11,22 @@ dtrain = df.sample(frac=0.8, random_state=32)
 dtest = df.drop(dtrain.index)
 
 # --- TRIM TRAIN AND TEST SET :
+"""
 tr = dtrain[["Calories", "TransFat"]]
 ytr = dtrain["Brand"]
 ts = dtest[["Calories", "TransFat"]]
 dft = df[["Calories", "TransFat"]]
+"""
+tr = dtrain[["Calories", "Carbohydrates"]]
+ytr = dtrain["Brand"]
+ts = dtest[["Calories", "Carbohydrates"]]
+dft = df[["Calories", "Carbohydrates"]]
+"""
+tr = dtrain[["Calories", "TotalFat"]]
+ytr = dtrain["Brand"]
+ts = dtest[["Calories", "TotalFat"]]
+dft = df[["Calories", "TotalFat"]]
+"""
 
 # --- ADD INTERCEPT 
 #tr = tr.assign(ones=pd.Series([1]*len(tr), index=tr.index))
