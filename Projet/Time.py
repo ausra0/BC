f0b323ef56e0fe4a4a65124314f3eff334cd9ab4
ausra0 <aushra.pogozelskyte@gmail.com>
2018-06-12 11:05:38 +0200

    time function update

--- Projet/Time.py
+++ Projet/Time.py
@@ -2,17 +2,19 @@ import sys, os
 import numpy as np 
 import pickle 
 
-n = 20
+n = 10
 t = np.zeros(n)
+
+filename = 'Mod12'
 
 for i in range(0, n): 
     # run the file 
     os.system('python '+filename+'.py')
 
     # get time from dump file 
-    with open("./times/M3.dat", "rb") as file:
+    with open("./times/"+filename+".dat", "rb") as file:
         t[i] = pickle.load(file)
 
-tim  = mean(t)
+tim  = np.mean(t)
 print('Benchmark for '+filename)
 print(tim)
