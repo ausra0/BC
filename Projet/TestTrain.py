8f186f3446358c55ed1f0d0dac73af8cc434178f
ausra0 <aushra.pogozelskyte@gmail.com>
2018-06-14 18:48:34 +0200

    Update for validation set

--- Projet/TestTrain.py
+++ Projet/TestTrain.py
@@ -5,6 +5,8 @@ from TransformData import *
 import numpy as np
 import numpy.random as rdm 
 
+#rdm.seed(32)
+
 # --- MAIN()
 # cut the dataset
 dtrain = df.sample(frac=0.8, random_state=32)
@@ -15,18 +17,28 @@ dtest = df.drop(dtrain.index)
 tr = dtrain[["Calories", "TransFat"]]
 ytr = dtrain["Brand"]
 ts = dtest[["Calories", "TransFat"]]
+yts = dtest['Brand']
 dft = df[["Calories", "TransFat"]]
 """
 tr = dtrain[["Calories", "Carbohydrates"]]
 ytr = dtrain["Brand"]
 ts = dtest[["Calories", "Carbohydrates"]]
+yts = dtest['Brand']
 dft = df[["Calories", "Carbohydrates"]]
 """
 tr = dtrain[["Calories", "TotalFat"]]
 ytr = dtrain["Brand"]
 ts = dtest[["Calories", "TotalFat"]]
+yts = dtest['Brand']
 dft = df[["Calories", "TotalFat"]]
 """
+
+# --- CREATE OUTLIERS DATASETS 
+fract = 0.1 # fraction of outliers 
+
+victims = ytr.sample(frac = fract, random_state = 32).index
+ytrout = ytr
+ytrout.loc[victims] = -1*ytrout.loc[victims]
 
 # --- ADD INTERCEPT 
 #tr = tr.assign(ones=pd.Series([1]*len(tr), index=tr.index))
